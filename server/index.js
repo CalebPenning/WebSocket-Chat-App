68804b03e4c281c5624fe2ecd086f27c0ed566e3
calebpenning <calebpenning@mail.com>
2021-07-07 00:06:59 -0700

    modified ./server/index.js and Login component

--- server/index.js
+++ server/index.js
@@ -11,6 +11,14 @@ const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
 
+io.on('connection', (socket) => {
+    console.log("New connection.")
+
+    socket.on('disconnect', () => {
+        console.log("User has left.")
+    })
+})
+
 app.use(router)
 
 server.listen(PORT, () => console.log(`Server has been started on port ${PORT}`))
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,24 @@
+import { Link } from 'react-router-dom'
+
 const Login = () => {
+    const [name, setName] = useState("")
+    const [room, setRoom] = useState("")
+
     return (
-        <h1>Login</h1>
+        <div className="outerContainer">
+            <div className="innerContainer">
+                <h1 className="heading">Login to Chat</h1>
+                <div>
+                    <input placeholder="Name" className="joinInput" type="text" onChange={(e) => setName(e.target.value)} />
+                </div>
+                <div>
+                    <input placeholder="Room" className="joinInput mt-20" type="text" onChange={(e) => setRoom(e.target.value)} />
+                </div>
+                <Link onClick={e => (!name || !room) ? e.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
+                    <button className="button mt-20" type="submit">Login</button>
+                </Link>
+            </div>
+        </div>
     )
 }
 
