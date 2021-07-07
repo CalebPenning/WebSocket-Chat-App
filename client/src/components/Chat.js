061c44ec39406d193f228b7519e7e2d21410435a
calebpenning <calebpenning@mail.com>
2021-07-07 12:36:19 -0700

    server is up and running + sockets are watching endpoints so log messages are coming through in real time :)

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,4 +1,27 @@
-const Chat = () => {
+import { useState, useEffect } from "react"
+import queryString from "query-string"
+import io from 'socket.io-client'
+
+let socket
+
+const Chat = ({ location }) => {
+    const [name, setName] = useState("")
+    const [room, setRoom] = useState("")
+    const ENDPOINT = "localhost:3001"
+
+    useEffect(() => {
+        // get query params and values, store in object
+        const { name, room } = queryString.parse(location.search)
+
+        socket = io(ENDPOINT)
+        console.log(socket)
+
+        setName(name)
+        setRoom(room)
+
+        socket.emit('login', { name, room })
+    }, [ENDPOINT, location.search])
+
     return (
         <h1>Chat</h1>
     )
--- server/index.js
+++ server/index.js
@@ -2,17 +2,29 @@ const express = require('express')
 const socketio = require('socket.io')
 const http = require('http')
 const router = require('./router')
+const cors = require("cors")
 
 // get port from process, otherwise 3000
 const PORT = process.env.PORT || 3001
 
-// create our server and socketio instance
+// enable cors and pass our port to avoid a bajillion console error messages
+// I believe this is unnecessary with socketio version < 3
+let corsOptions = {
+    cors: true,
+    origins: [PORT]
+}
+
+// create our server and socketio instance, passing our corsOptions into io
 const app = express()
 const server = http.createServer(app)
-const io = socketio(server)
+const io = socketio(server, corsOptions)
 
 io.on('connection', (socket) => {
     console.log("New connection.")
+
+    socket.on("login", ({ name, room }) => {
+        console.log(name, room)
+    })
 
     socket.on('disconnect', () => {
         console.log("User has left.")
@@ -20,5 +32,6 @@ io.on('connection', (socket) => {
 })
 
 app.use(router)
+app.use(cors())
 
 server.listen(PORT, () => console.log(`Server has been started on port ${PORT}`))
