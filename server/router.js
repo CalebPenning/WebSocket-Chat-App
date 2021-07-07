28dabffb24f2db3817ce7b87b4712a8570403a2e
calebpenning <calebpenning@mail.com>
2021-07-07 11:43:27 -0700

    again with the file structure

--- server/router.js
+++ server/router.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = new express.Router()
 
 router.get("/", (req, res, next) => {
-    return res.send("Hello")
+    return res.send("hello")
 })
 
 module.exports = router
