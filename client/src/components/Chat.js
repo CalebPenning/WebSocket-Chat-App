8fff90ab0972dd11e98b7d75edd42b8584d0a2ce
calebpenning <calebpenning@mail.com>
2021-07-07 17:45:08 -0700

    deleted log

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -58,8 +58,6 @@ const Chat = ({ location }) => {
         }
     }
 
-    console.log(message, messages)
-
     return (
         <div className="outerContainer">
             <div className="container">
